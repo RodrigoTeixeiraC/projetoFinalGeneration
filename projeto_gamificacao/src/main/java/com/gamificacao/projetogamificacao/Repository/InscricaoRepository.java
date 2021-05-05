d83d1c470a35c7e021494599430590fc4c3b4798
RIVELINO SANTOS <80266863+ri-ve@users.noreply.github.com>
2021-05-05 19:42:54 -0300

    Controller inscricao

--- projeto_gamificacao/src/main/java/com/gamificacao/projetogamificacao/Repository/InscricaoRepository.java
+++ projeto_gamificacao/src/main/java/com/gamificacao/projetogamificacao/Repository/InscricaoRepository.java
@@ -1,11 +1,20 @@
 package com.gamificacao.projetogamificacao.Repository;
 
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gamificacao.projetogamificacao.Models.Inscricao;
 
+
 @Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
+
+	   
+
+
+	
 
 }
