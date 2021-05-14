c8f4b221334907957969a430dca8362cff6440ef
AllanDeyvison <allandeyvison007dias@hotmail.com>
2021-05-14 12:26:26 -0300

    Usuario service postagem quiz

---
+++ projetogamificacao/src/main/java/com/gamificacao/projetogamificacao/Models/Atividades.java
@@ -0,0 +1,69 @@
+package com.gamificacao.projetogamificacao.Models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Atividades {
+	
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id_atividades;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data_atividades = new java.sql.Date(System.currentTimeMillis());
+	
+	private String atividade;
+	
+	private String status;
+	
+	@ManyToOne
+	@JoinColumn (name = "idUsuario")
+	private Usuario usuarioAtividade;
+
+	public long getId_atividades() {
+		return id_atividades;
+	}
+
+	public void setId_atividades(long id_atividades) {
+		this.id_atividades = id_atividades;
+	}
+
+	public Date getData_atividades() {
+		return data_atividades;
+	}
+
+	public void setData_atividades(Date data_atividades) {
+		this.data_atividades = data_atividades;
+	}
+
+	public Usuario getUsuarioAtividade() {
+		return usuarioAtividade;
+	}
+
+	public void setUsuarioAtividade(Usuario usuarioAtividade) {
+		this.usuarioAtividade = usuarioAtividade;
+	}
+
+	public String getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(String atividade) {
+		this.atividade = atividade;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
--- projetogamificacao/src/main/java/com/gamificacao/projetogamificacao/Service/UsuarioService.java
+++ projetogamificacao/src/main/java/com/gamificacao/projetogamificacao/Service/UsuarioService.java
@@ -1,86 +1,126 @@
 package com.gamificacao.projetogamificacao.Service;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.gamificacao.projetogamificacao.Models.Atividades;
 import com.gamificacao.projetogamificacao.Models.Grupo;
+import com.gamificacao.projetogamificacao.Models.InscricaoGrupo;
+import com.gamificacao.projetogamificacao.Models.PostagemQuiz;
 import com.gamificacao.projetogamificacao.Models.Usuario;
 import com.gamificacao.projetogamificacao.Models.UsuarioLogin;
 import com.gamificacao.projetogamificacao.Repository.GrupoRepository;
+import com.gamificacao.projetogamificacao.Repository.PostagemQuizRepository;
 import com.gamificacao.projetogamificacao.Repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	@Autowired
 	private GrupoRepository repositoryGrupo;
 	
-	public Optional<Usuario> cadastrarUsuario (Usuario usuario){
-		
+	@Autowired 
+	private PostagemQuizRepository PostQuizRepository;
+
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
 		Optional<Usuario> usuarioExistente = repository.findByUsuarioOrEmail(usuario.getUsuario(), usuario.getEmail());
-		
+
 		if (usuarioExistente.isPresent()) {
 			return Optional.empty();
-		}
-		else {
+		} else {
 			BCryptPasswordEncoder ecoder = new BCryptPasswordEncoder();
-			
+
 			String senhaEcoder = ecoder.encode(usuario.getSenha());
 			usuario.setSenha(senhaEcoder);
-			
+
 			return Optional.ofNullable(repository.save(usuario));
 		}
 	}
-	
-	public Optional<UsuarioLogin> Logar (Optional<UsuarioLogin> user){
+
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuarioOrEmail(user.get().getUsuario(), user.get().getUsuario());
-		
+
 		if (usuario.isPresent()) {
-			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha()));
-			
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha()))
+				;
+
 			String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 			byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-			String authHeader = "Basic " + new String (encodeAuth);
-			
+			String authHeader = "Basic " + new String(encodeAuth);
+
 			user.get().setToken(authHeader);
-			
+
 			return user;
 		}
 		return null;
 	}
-	
-	
+
 	/**
 	 * Cria um Grupo e seta o usuario existente como seu criador
-	 * @param	novoGrupo uma Entidade Grupo
-	 * @param	idUsuario tipo Long
-	 * @return	Optional com entidade usuario, caso usuario existente ou um Optional vazio
-	 * @since	1.0
-	 * @author	Rive
+	 * 
+	 * @param novoGrupo uma Entidade Grupo
+	 * @param idUsuario tipo Long
+	 * @return Optional com entidade usuario, caso usuario existente ou um Optional
+	 *         vazio
+	 * @since 1.0
+	 * @author Rive
 	 */
-	public Optional<Usuario> criarGrupo(Grupo novoGrupo, Long idUsuario){
-		return repository.findById(idUsuario)
-				.map(usuarioExistente -> {
-					novoGrupo.setCriador(usuarioExistente);
-					repositoryGrupo.save(novoGrupo);
-					return repository.findById(idUsuario);
-				}).orElse(Optional.empty());
+	public Optional<Usuario> criarGrupo(Grupo novoGrupo, Long idUsuario) {
+		return repository.findById(idUsuario).map(usuarioExistente -> {
+			novoGrupo.setCriador(usuarioExistente);
+			repositoryGrupo.save(novoGrupo);
+			return repository.findById(idUsuario);
+		}).orElse(Optional.empty());
 	}
-	public Optional<Usuario> aceitarnoGrupo(Grupo novointegrante, Long idGrupo){
-		return repository.findById(idGrupo)
-				.map(GrupoExistente -> {
-					novointegrante.setCriador(GrupoExistente);
-					repositoryGrupo.save(novointegrante);
-					return repository.findById(idGrupo);
-				}).orElse(Optional.empty());
+
+	public Optional<Usuario> aceitarnoGrupo(Grupo novointegrante, Long idGrupo) {
+		return repository.findById(idGrupo).map(GrupoExistente -> {
+			novointegrante.setCriador(GrupoExistente);
+			repositoryGrupo.save(novointegrante);
+			return repository.findById(idGrupo);
+		}).orElse(Optional.empty());
+	}
+
+	public List<Atividades> buscarAtividades(Usuario usuario) {
+
+		Set<Usuario> listaAmigos = usuario.getClan();
+		List<Atividades> atividades = usuario.getAtividadesUsuario();
+
+		for (Usuario i : listaAmigos) {
+
+			for (Atividades x : atividades) {
+
+				return atividades;
+			}
+		}
+		return null;
+	}
+	
+
+	public Optional<List<PostagemQuiz>> buscarPostQuiz (Usuario usuario){
+	
+		List<InscricaoGrupo> gruposInscritos = usuario.getListaInscricaoUG();
+		
+		gruposInscritos.stream()
+		.map(grupo -> 
+			grupo.getGrupoInscricao().getListaPostQuiz()
+				.stream()
+				.map(postQuiz -> {
+					postQuiz.getId();
+					return PostQuizRepository.findById(postQuiz);
+					}).orElse(Optional.empty())).orElse(Optional.empty());
+		
 	}
 }
